391b8ab8ffb9c30193f2d698310a79ade9306eac
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2022-03-08 12:44:13 +0100

    mover lineascomentadas

--- luces/luces1/luces1.ino
+++ luces/luces1/luces1.ino
@@ -9,10 +9,15 @@
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led1,OUTPUT);//digitalWrite(led1,HIGH);
-  pinMode(led2,OUTPUT);//digitalWrite(led2,HIGH);
-  pinMode(led3,OUTPUT);//digitalWrite(led3,HIGH);
-  pinMode(led4,OUTPUT);//digitalWrite(led4,HIGH);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+
+  //digitalWrite(led1,HIGH);
+  //digitalWrite(led2,HIGH);
+  //digitalWrite(led3,HIGH);
+  //digitalWrite(led4,HIGH);
 }
 
 void loop() {
