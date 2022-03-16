ac3035155b633097f2c82cbba6e7237e9b6bdb31
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2022-03-17 00:02:12 +0100

    Update luces1.ino
    comprobado en el arduino

--- luces/luces1/luces1.ino
+++ luces/luces1/luces1.ino
@@ -2,10 +2,10 @@
  * Este código se encarga de la secuencia aleatoria de lucecitas
 */
 
-#define led1 9
-#define led2 10
-#define led3 11
-#define led4 12
+#define led1 9 // rojo
+#define led2 10 // verde
+#define led3 11 // azul
+#define led4 12 // blanco
 
 void setup() {
   Serial.begin(9600);
@@ -29,26 +29,29 @@ void loop() {
   int pausa=1000; // tiempo de pausa entre secuencias
   int leds[]={led1,led2,led3,led4};
   int secuencia[]={0,0,0,0,0};
-  r=random(0,4); // genera un numero aleatorio del 0 al 3
-  Serial.println(r);
+//  r=random(0,4); // genera un numero aleatorio del 0 al 3
+//  Serial.println(r);
 
   for(j=0;j<n;j++){ // bucle que genera la secuencia, se repite infinitas veces
+
+    r=random(0,4);
     
     for(i=0;i<sizeof(leds)/sizeof(int);i++){ // bucle que enciende una lucecita de las 4, se repite 5 veces dentro de este
       if(r==i){
         digitalWrite(leds[i],LOW); // low = encendido
         delay(t);
         digitalWrite(leds[i],HIGH);
+        delay(100);
 
 //        secuencia[j]=(i+1);
 //        Serial.println(secuencia[j]);
       }
+      
       else{
         digitalWrite(leds[i],HIGH);
       }
-    }
-    
-  }
 
+    }
+  }
   delay(pausa);
 }
