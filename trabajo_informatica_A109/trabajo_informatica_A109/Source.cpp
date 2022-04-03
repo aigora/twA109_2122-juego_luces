1f9238c64d58a8392dafbb33b95ec97c78a5d82e
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2022-04-03 22:57:06 +0200

    añadir función y comentarios

--- trabajo_informatica_A109/trabajo_informatica_A109/Source.cpp
+++ trabajo_informatica_A109/trabajo_informatica_A109/Source.cpp
@@ -1,11 +1,13 @@
 
-/* Este código sirve para establecer la comunicación por cable con Arduino */
+/* Este código sirve para establecer la comunicación por cable con Arduino
+ y contiene las principales funciones de nuestro proyecto*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include "SerialClass/SerialClass.h"
 
+void verificar(Serial*, char*);
 void menu(void);
 void ficheros(void);
 void control_luces(void);
@@ -18,11 +20,25 @@ void main() {
 	setlocale(LC_ALL, "es-ES");
 
 	int opcion_menu;
+	Arduino = new Serial(puerto);
+
+	verificar(Arduino, puerto);
 
 	menu();
 	ficheros();
 	control_luces();
 
+}
+
+/* Verifica que se ha establecido correctamente la conexión */
+
+void verificar(Serial*Arduino, char*puerto) {
+	if (Arduino->IsConnected()) {
+		;
+	}
+	else {
+		printf("\nNo se ha podido conectar con el Arduino\n");
+	}
 }
 
 /* Menú principal de la aplicación */
