dab628a23c6d88290de7275054c8a3247cce1a42
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2022-05-02 18:06:27 +0200

    Update luces.ino

--- luces/luces.ino
+++ luces/luces.ino
@@ -9,27 +9,24 @@
 #define led3 11
 #define led4 12
 
-#define led5r 3
-#define led5g 4
-#define led5b 5
-
 void setup() {
   Serial.begin(9600);
   while(!Serial){
     ;
   }
+
+  Serial.println("INICIANDO PROGRAMA . . .");
+  
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
   pinMode(led3,OUTPUT);
   pinMode(led4,OUTPUT);
-  pinMode(led5r,OUTPUT);
-  pinMode(led5g,OUTPUT);
-  pinMode(led5b,OUTPUT);
 
   //digitalWrite(led1,HIGH);
   //digitalWrite(led2,HIGH);
   //digitalWrite(led3,HIGH);
   //digitalWrite(led4,HIGH);
+  
 }
 
 void loop() {
@@ -41,10 +38,6 @@ void loop() {
   int pausa=5000; // tiempo entre niveles
   int leds[]={led1,led2,led3,led4};
   int secuencia[]={0,0,0,0,0};
-
-  digitalWrite(led5r,0);
-  digitalWrite(led5g,0);
-  digitalWrite(led5b,0);
 
   /* bucle que genera la secuencia, se repite infinitas veces */
   for(j=0;j<n;j++){
@@ -70,18 +63,12 @@ void loop() {
   } // fin de la secuencia de luces
   //Serial.print('\n');
 
-  digitalWrite(led5r,255);
-  digitalWrite(led5g,0);
-  digitalWrite(led5b,0);
-
   /* envío de los datos de la secuencia al puerto serie: */
   if(Serial.available()>0){
     String str=Serial.readStringUntil('\n');
 
     if(str.compareTo("GET_SECUENCIA")==0){
-      digitalWrite(led5r,0);
-      digitalWrite(led5g,255);
-      digitalWrite(led5b,0);
+
       str="VALORES: ";
       for(i=0;i<sizeof(secuencia)/sizeof(int);i++){
         str.concat(secuencia[i]);
@@ -92,7 +79,6 @@ void loop() {
       Serial.println("COMANDO DESCONOCIDO");
     }
   }
-  
 
   /* espera 5 segundos hasta iniciar el siguiente nivel */
   delay(pausa);
