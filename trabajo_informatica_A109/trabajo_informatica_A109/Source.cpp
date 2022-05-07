52f13ce6b20b76b1821a4f0e86b8ac3cc76504f2
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2022-05-07 13:23:05 +0200

    Update Source.cpp

--- trabajo_informatica_A109/trabajo_informatica_A109/Source.cpp
+++ trabajo_informatica_A109/trabajo_informatica_A109/Source.cpp
@@ -44,7 +44,7 @@ int compara(int*, int*);
 int puntaje(int);
 bool game_over(int);
 int get_secuencia(Serial*,char*);
-int*transform_secuencia(char*);
+void transform_secuencia(char*,int*);
 void listado_jugadores(Jugador[], int);
 int  alta_jugador(Jugador[], int);
 void crear_fichero_txt(Jugador[], int);
@@ -279,13 +279,16 @@ void control_luces(Serial*Arduino,int*secuencia_luces,int*secuencia_jugador,int 
 
 /* Compara secuencias, devuelve 1 si son iguales, 0 si distintas */
 int compara(int* s1, int* s2) {
+	
 	int*i=(int*)malloc(sizeof(int));
+
 	for (*i = 0; *i < DIM; *i++) {
 		if (s1[*i] != s2[*i]){
 			free(i);
 			return 0;
 		}
 	}
+	
 	free(i);
 	return 1;
 }
@@ -310,7 +313,7 @@ int get_secuencia(Serial*Arduino,char*mensaje_recibido){
 	while(!Arduino->WriteData((char*)"GET_SECUENCIA\n",MAX_BUFFER*sizeof(char))){
 		;
 	}
-	//Sleep(PAUSA_MS);
+	Sleep(PAUSA_MS);
 
 	bytes=Arduino->ReadData(mensaje_recibido,sizeof(char)*MAX_BUFFER-1);
 	while(bytes>0){
@@ -329,41 +332,51 @@ int get_secuencia(Serial*Arduino,char*mensaje_recibido){
 }
 
 /* convierte el vector enviado por arduino en un vector numérico con los valores */
-int*transform_secuencia(char*mensaje_recibido){
+void transform_secuencia(char*mensaje_recibido, int s[DIM]){
 
-	int*k=(int*)malloc(sizeof(int));
-	int s[DIM];
+	int k;
 
-	for(*k=0;*k<sizeof(mensaje_recibido)/sizeof(char);*k++){
-		if(mensaje_recibido[*k]=='1') s[*k]=1;
-		if(mensaje_recibido[*k]=='2') s[*k]=2;
-		if(mensaje_recibido[*k]=='3') s[*k]=3;
-		if(mensaje_recibido[*k]=='4') s[*k]=4;
-		else s[*k]=0;
+	for (k = 0; k < DIM; k++) {
+		if (mensaje_recibido[k] == '1') s[k] = 1;
+		else if (mensaje_recibido[k] == '2') s[k] = 2;
+		else if (mensaje_recibido[k] == '3') s[k] = 3;
+		else s[k] = 4;
 	}
-	
-	free(k);
-	return s;
+
 }
 
 /* crea un vector a partir de las teclas pulsadas por el jugador */
-void get_secuencia_jugador(int*s_jugador) {
+void get_secuencia_jugador(int s_jugador[DIM]) {
 
 	int i = 0;
-	char t;
+	char t='0';
 
-	printf("\n repita la secuencia de luces en el teclado . . . \n");
+	printf("\n repita la secuencia en el teclado . . . \n");
+
 	for(i=0;i<DIM;i++){
+
 		scanf_s("%c",&t);
 
-		if(t=='A'||t=='a') s_jugador[i]=1;
-		if(t=='W'||t=='w') s_jugador[i]=2;
-		if(t=='S'||t=='s') s_jugador[i]=3;
-		if(t=='D'||t=='d') s_jugador[i]=4;
+		if(t=='A'||t=='a'){
+			 s_jugador[i]=1;
+		}
+		else if(t=='W'||t=='w'){
+			s_jugador[i]=2;
+		}
+		else if(t=='S'||t=='s'){
+			s_jugador[i]=3;
+		}
+		else if(t=='D'||t=='d'){
+			s_jugador[i]=4;
+		}
 
 		else{
 			s_jugador[i]=0;
 		}
 	}
+	fflush(stdin);
+
+	printf("%d",s_jugador[i]);
+	fflush(stdout);
 
 }
